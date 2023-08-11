dd11a8849da19d28346118f49b0cd92ff091f01f
karthikpeddi14 <128394123+karthikpeddi14@users.noreply.github.com>
2023-08-11 22:41:25 +0530

    Update Matrix Chain Multiplication.cpp

--- Matrix Chain Multiplication.cpp
+++ Matrix Chain Multiplication.cpp
@@ -16,3 +16,23 @@ int matrixChainMultiplication(int* arr, int n) {
     return f(arr,0,n,dp);
 }
 // recur and memo
+
+// tabulation
+#include <bits/stdc++.h>
+int matrixChainMultiplication(int* arr, int n) {
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    
+    for(int i=n-1 ; i>=0; i--)
+    {
+        for(int j=i+2 ; j<=n ; j++)
+        {
+            int ans = INT_MAX;
+            for(int k=i+1; k<j ; k++){
+                ans = min(ans,dp[i][k]+dp[k][j]+arr[i]*arr[k]*arr[j]);
+            }
+            dp[i][j] = ans;
+        }
+    }
+    return dp[0][n];
+}
+// tabulation
